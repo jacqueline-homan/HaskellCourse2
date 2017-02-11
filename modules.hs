d4378ef527fb3057fdce43185d0613c6920487b2
jacqueline-homan <jacquelinehoman7@gmail.com>
2017-02-10 23:09:59 -0500

    modules in Haskell

---
+++ modules.hs
@@ -0,0 +1,11 @@
+{-import Data.Set 
+import qualified Data.Set
+import qualified Data.Sequence
+-}
+import qualified Data.Sequence as Seq 
+import Data.Set (empty, size)
+import Data.Maybe (Maybe (..))
+import Control.Monad (Monad, return)
+import Data.Set ()
+
+import Data.Set hiding (empty, size)
